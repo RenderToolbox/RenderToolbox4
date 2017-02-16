ba6addb362b45978e1ac46ea66630d1c6ff6f543
Ben Heasly <benjamin.heasly@gmail.com>
2017-02-15 21:29:46 -0500

    Revert "Revert "Performance tweaks""
    This reverts commit 1765396fdf335e0e09ed07d92297d5476ca9a862.

---
+++ ExampleScenes/IsetbioExamples/Car/rtbMakeCar.m
@@ -0,0 +1,130 @@
+% Render a Mercedes SLS and display as an isetbio optical image.
+%
+% Henryk Blasinski
+
+
+%% Initialize.
+
+% tbUse('isetbio');
+
+close all;
+clear;
+clc;
+
+ieInit;
+
+
+%% Choose simulation parameters.
+filmDiag = 20;
+targetDistance = 5000;
+nSamples = 256;
+
+
+%% Choose Batch Render Options.
+hints.imageWidth = 320;
+hints.imageHeight = 240;
+hints.renderer = 'PBRT';
+hints.recipeName = 'Car';
+hints.batchRenderStrategy = RtbAssimpStrategy(hints);
+hints.batchRenderStrategy.remodelPerConditionAfterFunction = @rtbMakeCarMexximpRemodeller;
+hints.batchRenderStrategy.converter.remodelAfterMappingsFunction = @rtbMakeCarPBRTRemodeller;
+
+% change the docker container
+hints.batchRenderStrategy.renderer.pbrt.dockerImage = 'vistalab/pbrt-v2-spectral';
+
+parentSceneFile = fullfile(rtbRoot(), 'ExampleScenes', 'IsetbioExamples', ...
+    'Car', 'Models', 'Mercedes SLS', 'sls_amg.obj');
+
+resources = rtbWorkingFolder( ...
+    'folderName', 'resources', ...
+    'hints', hints);
+
+conditionsFile = fullfile(resources, 'rtbMakeCarConditions.txt');
+
+
+%% Save data to resources folder.
+[waves, daylight] = rtbReadSpectrum('D65.spd');
+rtbWriteSpectrumFile(waves,1e4*daylight,fullfile(resources,'SunLight.spd'));
+
+
+%% Load the scene.
+[scene, elements] = mexximpCleanImport(parentSceneFile,...
+    'ignoreRootTransform',true,...
+    'flipUVs',false,...
+    'exrToolsImage','hblasins/imagemagic-docker',...
+    'convertToLeftHanded',true, ...
+    'workingFolder', resources);
+
+
+%% Write conditions file and scene files.
+condition = {'clear'};
+daylight = {'off'};
+headlights = {'on'};
+taillights = {'on'};
+nConditions = length(targetDistance)*length(condition);
+
+names = {'imageName','mode','pixelSamples','volumeStep','filmDist','filmDiag','cameraDistance','daylight','headlights','taillights'};
+
+values = cell(nConditions,numel(names));
+cntr = 1;
+for d=1:length(daylight)
+    for h=1:length(headlights)
+        for t=1:length(taillights)
+            for td = targetDistance
+                for c=1:length(condition)
+                    
+                    % Generate condition entries
+                    values(cntr,1) = {'someName'};
+                    values(cntr,2) = condition(c);
+                    values(cntr,3) = num2cell(nSamples,1);
+                    values(cntr,4) = num2cell(50,1);
+                    values(cntr,5) = num2cell(((0.8*filmDiag*td)/5000),1);
+                    values(cntr,6) = num2cell(filmDiag,1);
+                    values(cntr,7) = num2cell(td,1);
+                    values(cntr,8) = daylight(d);
+                    values(cntr,9) = headlights(h);
+                    values(cntr,10) = taillights(t);
+                    
+                    cntr = cntr+1;
+                end
+            end
+        end
+    end
+end
+
+rtbWriteConditionsFile(conditionsFile,names,values);
+
+nativeSceneFiles = rtbMakeSceneFiles(scene, 'hints', hints, ...
+    'conditionsFile',conditionsFile);
+
+
+%% Render!
+radianceDataFiles = rtbBatchRender(nativeSceneFiles, 'hints', hints);
+
+
+%% Display as an isetbio optical image.
+
+% TODO: Ideally, we want to use BuildOI here. Unfortunately, we don't have
+% the "oiParameters" structure anymore. In RTB4, this structure held
+% information about the film distance, diagonal, etc. We could then use
+% this information to fill in parameter info in the OI, such as the
+% F-number or the FOV. Now all that info is held in the remodeler. How
+% should we move that info out from the remodeler into the oi?
+
+for i=1:length(radianceDataFiles)
+    radianceData = load(radianceDataFiles{i});
+    photons = radianceData.multispectralImage;
+    
+    oiName = sprintf('%s_%i',hints.recipeName,i);
+    
+    % Create an oi
+    oi = oiCreate;
+    oi = initDefaultSpectrum(oi);
+    oi = oiSet(oi, 'photons', single(photons));
+    oi = oiSet(oi, 'name',oiName);
+    
+    vcAddAndSelectObject(oi);
+end
+
+oiWindow;
+
---
+++ ExampleScenes/IsetbioExamples/SponzaAtrium/rtbMakeSponzaAtrium.m
@@ -0,0 +1,226 @@
+%% Render the Sponza Atrium using RTB4
+
+% This script renders the Sponza Atrium (simple version) with different
+% light spectra and different camera types. The light comes from a large
+% rectangular area light on top of the atrium.
+
+% The for different renders are:
+% 1. D65 light rendered with short DOF (50mm lens)
+% 2. D65 light with long DOF (50 mm lens)
+% 3. Bright yellow light with a pinhole camera
+% 4. Dim blue light with a pinhole camera camera
+
+% Once rendered, it saves each multispectral image as an ISET optical image
+% and uses ISET to display the oi's. 
+
+% The Sponza model was imported into Blender, an Area Light mesh was added,
+% and then exported as a Collada file. In the future, we can probably
+% import the OBJ model directly and combine it with a model of the Area
+% Light.
+
+% 11/22/16 Bump maps are now working! There may be some weird UV mapping
+% issues with the texture on the columns - will sort out in the future.
+
+% Approximate times to render:
+%
+% Size = [400x270]
+%   Condition 3 (only) --> 1024 pixel samples
+% Total Time: ~400 seconds
+%
+% Size = [150x100]
+%   Condition 1 --> (4096 pixel samples)
+%   Condition 2 --> (4096 pixel samples)
+%   Condition 3 --> (1024 pixel samples)
+%   Condition 4 --> (1024 pixel samples)
+% Total time: 638 sec ~ 10 min
+
+% Size = [400x270]
+%   Condition 1 --> (4096 pixel samples)
+%   Condition 2 --> (4096 pixel samples)
+%   Condition 3 --> (1024 pixel samples)
+%   Condition 4 --> (1024 pixel samples)
+% Total time: 3891 sec ~ 1 hour
+
+% Size = [600x400]
+%   Condition 1 --> (8192 pixel samples)
+%   Condition 2 --> (8192 pixel samples)
+%   Condition 3 --> (4096 pixel samples)
+%   Condition 4 --> (4096 pixel samples)
+% Total time: 19443 sec ~ 5 hours
+
+% TL
+
+%% Initialize
+
+% tbUse('isetbio')
+
+clear; close all;
+ieInit;
+
+%% Choose batch renderer options.
+hints.imageWidth = 150;
+hints.imageHeight = 100;
+hints.recipeName = 'rtbMakeSponzaAtrium';
+
+hints.renderer = 'PBRT';
+hints.batchRenderStrategy = RtbAssimpStrategy(hints);
+
+% We use "rtbSponzaPBRTRemodeler" to add all the parameters specific to
+% PBRT. These include the cameras (e.g. pinhole, realisticDiffraction),
+% number of pixel samples, and light spectra.
+% If we wanted to change things like camera position and move meshes
+% around, we would specify another file like "rtbSponzaMexximpRemodeler"
+% instead. 
+hints.batchRenderStrategy.converter.remodelAfterMappingsFunction = @rtbSponzaPBRTRemodeler;
+
+% Change the docker container to our version of PBRT-spectral
+hints.batchRenderStrategy.renderer.pbrt.dockerImage = 'vistalab/pbrt-v2-spectral';
+
+resourceFolder = rtbWorkingFolder( ...
+    'folderName', 'resources',...
+    'hints', hints);
+
+%% Load scene
+
+% The following scenefile must be a full path.
+sceneFile = fullfile(rtbRoot(), 'ExampleScenes', 'IsetbioExamples', ...
+    'SponzaAtrium', 'Data', 'SponzaScaled-YForwardZUp.obj');
+
+[scene, elements] = mexximpCleanImport(sceneFile,...
+    'flipUVs',true,...
+    'imagemagicImage','hblasins/imagemagic-docker',...
+    'toReplace',{'jpg','tiff'},...
+    'targetFormat','exr', ...
+    'strictMatching', true, ...
+    'workingFolder', resourceFolder);
+                                    
+
+%% Move camera to a new position (in millimeters)
+% We manually place it in a good position using the LookAt command.
+
+% Add a camera
+scene = mexximpCentralizeCamera(scene);
+
+% Move camera
+% These position were determined by looking at the Blender model
+from = [537 8265 4050];
+to = [0 -7265 1031];
+up = [0 0 1];
+mexximpLookAt(from, to, up);
+
+cameraTransform = mexximpLookAt(from, to, up);
+cameraNodeSelector = strcmp(scene.cameras.name, {scene.rootNode.children.name});
+scene.rootNode.children(cameraNodeSelector).transformation = cameraTransform;
+
+%% Write the spectrum files.
+
+workingFolder = rtbWorkingFolder('hints', hints);
+
+% Get basis CIE daylight basis vectors
+cieInfo = load('B_cieday');
+
+% Load up D65
+[wls,spd] = rtbReadSpectrum('D65.spd');
+rtbWriteSpectrumFile(wls, spd, fullfile(workingFolder, 'D65.spd'));
+
+% Make a bright yellow sun
+temp = 4000;
+scale = 1;
+spd = scale * GenerateCIEDay(temp, cieInfo.B_cieday);
+wls = SToWls(cieInfo.S_cieday);
+rtbWriteSpectrumFile(wls, spd, fullfile(workingFolder, 'BrightYellowSun.spd'));
+
+% Make a dimmer blue sky
+temp = 10000;
+scale = 0.001;
+spd = scale * GenerateCIEDay(temp, cieInfo.B_cieday);
+wls = SToWls(cieInfo.S_cieday);
+rtbWriteSpectrumFile(wls, spd, fullfile(workingFolder, 'DimBlueSky.spd'));
+
+%% Write conditions and generate scene files
+
+% We will have 4 conditions total
+% 1. D65 with short DOF (50mm lens)
+% 2. D65 with long DOF (50 mm lens)
+% 3. Bright yellow light with a pinhole
+% 4. Dim blue light with a pinhole camera
+
+%{
+names = {'lightSpectrum','apertureDiameter','cameraType','numSamples'};
+nConditions = 4;
+values = cell(nConditions,numel(names));
+values(1,:) = {'D65.spd',17,'realistic',8192}; % First light condition
+values(2,:) = {'D65.spd',7,'realistic',8192};
+values(3,:) = {'BrightYellowSun.spd',0,'pinhole',4096}; % Second light condition
+values(4,:) = {'DimBlueSky.spd',0,'pinhole',4096}; % Third light condition
+%}
+
+% ----------
+% Only render condition 3 for fast debugging.
+% 400x270, 1024 pixel samples --> 260 seconds
+names = {'lightSpectrum','apertureDiameter','cameraType','numSamples'};
+nConditions = 1;
+values = cell(nConditions,numel(names));
+values(1,:) = {'BrightYellowSun.spd',0,'pinhole',1024}; 
+% ----------
+
+% Write the parameters in a conditions file. 
+conditionsFile = 'SponzaConditions.txt';
+conditionsPath = fullfile(resourceFolder, conditionsFile);
+rtbWriteConditionsFile(conditionsPath, names, values);
+
+% Make the PBRT scene file.
+nativeSceneFiles = rtbMakeSceneFiles(scene,'hints', hints,'conditionsFile',conditionsPath);
+
+%% Move lens file into resource folder.
+% TODO: Any chance we can do this automatically?
+lensFilePath = fullfile(rtbRoot(), 'ExampleScenes', 'IsetbioExamples', ...
+    'SponzaAtrium', 'dgauss.50mm.dat'); 
+copyfile(lensFilePath, workingFolder);
+
+%% Render!
+radianceDataFiles = rtbBatchRender(nativeSceneFiles, ...
+    'hints', hints);
+
+%% View as an OI
+
+% TODO: Ideally, we want to use BuildOI here. Unfortunately, we don't have
+% the "oiParameters" structure anymore. In RTB4, this structure held
+% information about the film distance, diagonal, etc. We could then use
+% this information to fill in parameter info in the OI, such as the
+% F-number or the FOV. Now all that info is held in
+% "rtbSponzaPBRTRemodeler.m" How should we move that info out from the
+% remodeler into the oi? 
+
+renderingsFolder = rtbWorkingFolder( ...
+    'folderName', 'renderings',...
+    'hints', hints);
+
+% Load in rendered data
+for i = 1:nConditions
+    
+radianceData = load(radianceDataFiles{i});
+photons = radianceData.multispectralImage;
+
+oiName = sprintf('%s_%i',hints.recipeName,i);
+
+% Create an oi
+oi = oiCreate;
+oi = initDefaultSpectrum(oi);
+oi = oiSet(oi, 'photons', single(photons) * 10^13); % I believe scaling here is arbitrary.
+oi = oiSet(oi,'name',oiName);
+
+vcAddAndSelectObject(oi);
+
+% Save oi
+% TODO: Save rendering parameters somewhere?
+save(fullfile(renderingsFolder,sprintf('oi%i',i)),'oi');
+
+% Save RGB
+rgb = oiGet(oi,'rgb');
+imwrite(rgb,sprintf('%s.png',oiName))
+
+end
+
+oiWindow;
+
